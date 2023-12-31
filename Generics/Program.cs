f5c4860fdcf0a586381485391a5a314d9eceac3e
ripeplantain <irvingmanny@gmail.com>
2023-12-31 14:15:18 +0000

    generics added

---
+++ Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Equal = Calculator.AreEqual<int>(1, 2);
+            if (Equal)
+                Console.WriteLine("Equal");
+            else
+                Console.WriteLine("Not Equal");
+        }
+    }
+
+    public class Calculator
+    {
+        public static bool AreEqual<T>(T Value1, T Value2)
+        {
+            return Value1.Equals(Value2);
+        }
+    }
+}
