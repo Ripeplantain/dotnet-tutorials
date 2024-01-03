7f17cd9398956ebd5dadcef1854f4170815a0d9d
ripeplantain <irvingmanny@gmail.com>
2024-01-03 01:44:13 +0000

    Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+            numbers.Push(4);
+            numbers.Push(5);
+
+            // int lastNumber = numbers.Peek();
+            // Console.WriteLine("Last number: {0}", lastNumber);
+
+            int removedNumber = numbers.Pop();
+            Console.WriteLine("Removed number: {0}", removedNumber);
+        }
+    }
+}
